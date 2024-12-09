48734cddac72bc67a88b52f338e39a80af278492
Argel Hernandez <argel6767@gmail.com>
2024-12-08 23:29:57 -0500

    begin changes

--- src/data/seo.js
+++ src/data/seo.js
@@ -2,36 +2,29 @@ const SEO = [
 	{
 		page: "home",
 		description:
-			"I am a backend developer with expertise in Node.js. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies.",
-		keywords: ["Tharindu", "Tharindu N", "Tharindu Nayanajith"],
+			"I am a Student with a passion for computer science and software development. I love to learn and create.",
+		keywords: ["Argel", "Argel HA", "Argel Hernandez Amaya"],
 	},
 
 	{
 		page: "about",
 		description:
-			"I am a backend developer with expertise in Node.js. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies.",
-		keywords: ["Tharindu", "Tharindu N", "Tharindu Nayanajith"],
-	},
-
-	{
-		page: "articles",
-		description:
-			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
-		keywords: ["Tharindu", "Tharindu N", "Tharindu Nayanajith"],
+			"I am a CS student at Western New England University. I posses proficiency in multiple languages, frameworks, libraries, and tools. ",
+		keywords: ["Argel", "Argel HA", "Argel Hernandez Amaya"],
 	},
 
 	{
 		page: "projects",
 		description:
-			"I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to.",
-		keywords: ["Tharindu", "Tharindu N", "Tharindu Nayanajith"],
+			"I've worked on a variety of projects over the time in college to upskill in my own time and to improve as a developer.",
+		keywords: ["Argel", "Argel HA", "Argel Hernandez Amaya"],
 	},
 
 	{
 		page: "contact",
 		description:
-			"If you're interested in collaborating on a project, feel free to reach out to me. I'm always open to new ideas and opportunities.",
-		keywords: ["Tharindu", "Tharindu N", "Tharindu Nayanajith"],
+			"If you're interested in collaborating on a project, getting in touch, or to ask any questions, feel free to reach out to me. I'm always open to new ideas and opportunities.",
+		keywords: ["Argel", "Argel HA", "Argel Hernandez Amaya"],
 	},
 ];
 
--- src/data/user.js
+++ src/data/user.js
@@ -1,37 +1,29 @@
 const INFO = {
 	main: {
-		title: "Reactfolio by truethari",
-		name: "Tharindu N.",
-		email: "mail@tharindu.dev",
+		title: "Developer Portfolio",
+		name: "Argel Hernandez Amaya",
+		email: "argel6767@gmail.com",
 		logo: "../logo.png",
 	},
 
 	socials: {
-		twitter: "https://twitter.com/",
-		github: "https://github.com/",
-		linkedin: "https://linkedin.com/",
-		instagram: "https://instagram.com/",
-		stackoverflow: "https://stackoverflow.com/",
-		facebook: "https://facebook.com/",
+		github: "https://github.com/argel6767",
+		linkedin: "https://linkedin.com/in/argel-hernandez-amaya",
 	},
 
 	homepage: {
-		title: "Full-stack web and mobile app developer, and amateur astronaut.",
+		title: "Welcome to my Portfolio!",
 		description:
-			"I am a backend developer with expertise in Node.js. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a developer.",
+		"Hello! My name is Argel. I am student at Western New England University studying computer science. I love to program and am always learning to try to improve. Currently, I am bolstering my frontend skills with React and Tailwind CSS."
 	},
 
 	about: {
-		title: "I’m Tharindu N. I live in Sri Lanka, where I design the future.",
+		title: "A Little Bit About Me",
 		description:
-			"I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to. If you're interested in any of the projects I've worked on, please feel free to check out the code and suggest any improvements or enhancements you might have in mind. Collaborating with others is a great way to learn and grow, and I'm always open to new ideas and feedback.",
+		"I am proficient in Java, Python, HTML, CSS and Javascript. I have also created projects with both frontend and backend frameworks/libraries such as Spring Boot and React." +
+			" I love to workout and spend time with my friends when I am not programming."
 	},
 
-	articles: {
-		title: "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
-		description:
-			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
-	},
 
 	projects: [
 		{
